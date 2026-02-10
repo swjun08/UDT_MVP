359aeaffccca6c9e455dd244b7712ae1a8f155b7
Seojun Park <robinseojun0625@gmail.com>
2026-02-11 00:11:07 +0900

    Fix static file routing in production

--- udt_mvp/urls.py
+++ udt_mvp/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+import re
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -17,13 +18,36 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG or getattr(settings, "SERVE_STATIC_IN_APP", False):
+if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server or MVP app server
+    # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif getattr(settings, "SERVE_STATIC_IN_APP", False):
+    from django.urls import re_path
+    from django.views.static import serve
+
+    static_prefix = settings.STATIC_URL.strip("/")
+    media_prefix = settings.MEDIA_URL.strip("/")
+
+    if static_prefix:
+        urlpatterns += [
+            re_path(
+                rf"^{re.escape(static_prefix)}/(?P<path>.*)$",
+                serve,
+                {"document_root": settings.STATIC_ROOT},
+            )
+        ]
+    if media_prefix:
+        urlpatterns += [
+            re_path(
+                rf"^{re.escape(media_prefix)}/(?P<path>.*)$",
+                serve,
+                {"document_root": settings.MEDIA_ROOT},
+            )
+        ]
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
